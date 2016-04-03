271acd383079adcf48a7cf18305f0243e6560f05
Billy Cromb <billy.cromb@gmail.com>
2016-04-03 12:19:36 -0700

    Revert "Fixed integer bug and moved some files to make eclipse happy"
    Somehow eclipse deleted some code
    
    This reverts commit 3adb7ccf46136a4e89ec60b5cc214b042bc97da2.

--- test_jdbc/src/views/CustomersView.java
+++ test_jdbc/src/views/CustomersView.java
@@ -173,7 +173,197 @@ private void signup(){
 		    		String pCode1 = pCode.getText();
 
 				    	if(pCode1.isEmpty()){
-							JOptionPane.showMessageDialog(null, "Try Again!Wrong int clid1 = Integer.parseInt(clid.getText());
+							JOptionPane.showMessageDialog(null, "Try Again!Wrong PostalCode");
+
+				    	}
+				    	else{	
+				      String email1 = email.getText();
+				   // int classid1 = Integer.parseInt(classid.getText());
+				      JOptionPane.showMessageDialog(null, "Registered");        }
+				// TODO Auto-generated method stub
+				
+			}
+	    });
+		
+
+}
+private class thehandler implements ActionListener {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+			   if(e.getSource() == signup){
+				   signup();
+		              }
+
+			   
+			   else{
+				   if(e.getSource() == login){
+					try{
+					   String cid = JOptionPane.showInputDialog("enter cid");
+						int cids = Integer.parseInt(cid); 
+
+					    String answer = instance.login(cids);
+						JOptionPane.showMessageDialog(null, answer);
+
+					   
+					   
+						
+					        Integer.parseInt( cid );
+					    }
+					    catch( Exception e4 ) {
+					           JOptionPane.showMessageDialog(null, "No");}  
+				    //String cname1 = instance.getname(cid);
+				    //int answer1 = Integer.getInteger(answer);
+				  //  if(answer.isEmpty())
+				    //{JOptionPane.showMessageDialog(null, "No id exists");}
+				    //else{		*/
+					//int cids = Integer.parseInt(cid); 
+
+				    //String answer = instance.login(cids);
+					// JOptionPane.showMessageDialog(null, answer);
+
+					/* if(answer.isEmpty()){
+						 
+					
+							// TODO Auto-generated catch block
+							 JOptionPane.showMessageDialog(null, "Answer length cant be 0");
+						}*/
+					 //else {	
+			           //JOptionPane.showMessageDialog(null, answer);
+						 
+						  System.out.println("Button clicked");   
+						  
+						  JButton Updateinfo = new JButton("UPDATE CUSTOMER INFO");
+						  JButton addclass = new JButton("ADD CLASS");
+						  JButton deleteclass = new JButton("DELETE CLASS");
+						  //JButton  = new JButton();
+						  JFrame frame3 = new JFrame("Signup Form");
+						  JPanel panel3 = new JPanel();
+						  panel3.setLayout(new BoxLayout(panel3,BoxLayout.Y_AXIS));
+					    
+						panel3.add(BorderLayout.AFTER_LAST_LINE, Updateinfo);
+						panel3.add(BorderLayout.AFTER_LAST_LINE, addclass);
+						panel3.add(BorderLayout.AFTER_LAST_LINE, deleteclass);
+
+
+						frame3.add(panel3);
+						frame3.setSize(300, 300);
+						frame3.setVisible(true);
+						
+//updatebutton
+					Updateinfo.addActionListener(new ActionListener(){
+					    
+						@Override
+							public void actionPerformed(ActionEvent e) {
+								// TODO Auto-generated method stub JFrame frame2 = new JFrame("Signup Form");
+							    if(e.getSource() == Updateinfo){
+							    JFrame frame4 = new JFrame("Update Info");
+
+								JPanel panel4 = new JPanel();
+
+							    
+							   
+								
+								JTextField cid = new JTextField("Enter cid");
+								JTextField name = new JTextField("Enter Customer name");
+							    JTextField phone = new JTextField("Enter phone");
+							    JTextField sAddress = new JTextField("Enter Address");
+							    JTextField pCode = new JTextField("Enter postalcode");
+							    JTextField email = new JTextField("Enter email");
+								JButton Done = new JButton("Done");
+
+							   // panel4.add(cid);
+							   panel4.add(cid);
+								panel4.add(name);
+							    panel4.add(phone);
+							    panel4.add(sAddress);
+							    panel4.add(pCode);
+							    panel4.add(email);
+							    panel4.setLayout(new BoxLayout(panel4,BoxLayout.Y_AXIS));
+							    
+								panel4.add(BorderLayout.AFTER_LAST_LINE, Done);
+								frame4.add(panel4);
+								frame4.setSize(300, 300);
+								frame4.setVisible(true);
+								
+	//DOING DONE BUTTON NOW
+								Done.addActionListener(new ActionListener(){
+									@Override
+									public void actionPerformed(ActionEvent e) {
+										//if (e.getSource() == Done){
+											try{
+											int cid1 =Integer.getInteger(cid.getText());	
+										    int phone1 = Integer.parseInt(phone.getText());
+										     String Address = sAddress.getText();
+										     String pc = pCode.getText();
+										     String emailA = email.getText();}
+                                             
+										    
+										catch (Exception e1) {
+											
+										    System.out.println("Try Again!");
+											JOptionPane.showMessageDialog(null, "Try Again!Wrong Input");
+										}
+											int cid1 =Integer.getInteger(cid.getText());	
+										    String phone1 = (phone.getText());
+									        String name1 = name.getText();
+										    String saddress1 = sAddress.getText();
+								    		String pCode1 = pCode.getText();
+								    		String emailA = email.getText();
+										    	if(pCode1.isEmpty()){
+													JOptionPane.showMessageDialog(null, "Try Again!Wrong PostalCode");
+
+										    	}
+										    	//else{	
+										      String email1 = email.getText();
+										   // int classid1 = Integer.parseInt(classid.getText());
+										      String answer =instance.Updatecustomer(name1, phone1, saddress1, pCode1, emailA,cid1);
+										      JOptionPane.showMessageDialog(null, "Done"); }});       
+										
+									}}});
+								
+								
+								
+					//add button
+
+				 addclass.addActionListener(new ActionListener(){
+							@Override
+							public void actionPerformed(ActionEvent e) {
+								// TODO Auto-generated method stub
+								JOptionPane.showMessageDialog(null, "Try Again!Wrong Input");
+								}});
+						
+				//delete button
+				     deleteclass.addActionListener(new ActionListener(){
+						 @Override
+ 
+				   public void actionPerformed(ActionEvent e) {
+						// TODO Auto-generated method stub
+						 JFrame frame4 = new JFrame("Delete Info");
+
+							JPanel panel4 = new JPanel();
+
+						    
+						   
+							
+							JTextField cid = new JTextField("Enter cid");
+							
+						    JTextField clid = new JTextField("Enter classid");
+						   
+							JButton Delete = new JButton("Delete");
+							
+							try{
+								int cid1 =Integer.getInteger(cid.getText());	
+							    int clid1 = Integer.parseInt(clid.getText());}
+							    
+                                 
+							    
+							catch (Exception e1) {
+								
+							    System.out.println("Try Again!");
+								JOptionPane.showMessageDialog(null, "Try Again!Wrong Input");
+							}
+							int cid1 =Integer.getInteger(cid.getText());	
+						    int clid1 = Integer.parseInt(clid.getText());
 						   // panel4.add(cid);
 						   panel4.add(cid);
 							panel4.add(clid);
